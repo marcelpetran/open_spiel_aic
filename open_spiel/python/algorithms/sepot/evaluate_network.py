5cdd529611b88c58aee0f826d40e223028d2c6e6
kubicon3 <kubicon3@fel.cvut.cz>
2024-10-17 16:35:48 +0200

    Added postprocessing types to evaluation of the network

--- open_spiel/python/algorithms/sepot/evaluate_network.py
+++ open_spiel/python/algorithms/sepot/evaluate_network.py
@@ -3,7 +3,7 @@ import os
 import argparse
 import numpy as np
 import pickle
-
+import jax
 import time
 
 import open_spiel.python.algorithms.sepot.rnad_sepot as rnad
@@ -16,13 +16,43 @@ import pyspiel
 parser = argparse.ArgumentParser()
 # Experiments specific arguments
 
-parser.add_argument("--game_simulations", default=300, type=int, help="Amount of main iterations (each saves model)")
+parser.add_argument("--game_simulations", default=30, type=int, help="Amount of main iterations (each saves model)")
 parser.add_argument("--model_path", default="sepot_networks/dark_chess/rnad_666321", type=str, help="Length of each iteration in seconds") 
 parser.add_argument("--iterations_range", default=[200000, 210000, 5000], nargs="+", type=int, help="Ship sizes")
+parser.add_argument("--postprocessing", default=1, type=int, help="Postprocessing of the policy. 0 for none, 1 for thresholding, 2 for threshholding and discretization")
  
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 
 parser.add_argument("--evaluate_type", default="random", type=str, help="Type of evaluation. Could be random or older")
+
+def get_policy_from_network(network, state, postprocessing):
+  if postprocessing == 0:
+    env_step = network._batch_of_states_as_env_step([state])
+    probs = network._network_jit_apply(
+        network.params_target, env_step)
+    probs = jax.device_get(probs[0]).astype(np.float64)  # Squeeze out the 1-element batch.
+    probs = probs / np.sum(probs)
+    
+    return {
+        action: probs[action]
+        for action, valid in enumerate(jax.device_get(env_step.legal[0]))
+        if valid
+    }
+  elif postprocessing == 1:
+    env_step = network._batch_of_states_as_env_step([state])
+    probs = network._network_jit_apply(
+        network.params_target, env_step)
+    probs = jax.device_get(probs[0]).astype(np.float64)   # Squeeze out the 1-element batch.
+    probs_thresholded = np.where(probs > 0.02, probs, 0.0)
+    probs = probs / np.sum(probs) if np.sum(probs_thresholded) < 0.01 else probs_thresholded / np.sum(probs_thresholded)
+    return {
+        action: probs[action]
+        for action, valid in enumerate(jax.device_get(env_step.legal[0]))
+        if valid
+    }
+    pass
+  elif postprocessing == 2:
+    return network.action_probabilities(state)
 
 def evaluate_network_with_older(args):
   np_rng = np.random.RandomState(args.seed)
@@ -41,7 +71,7 @@ def evaluate_network_with_older(args):
         state = compared_model._game.new_initial_state()
         while not state.is_terminal(): 
           if state.current_player() >= 0: 
-            aps = models[state.current_player()].action_probabilities(state)
+            aps = get_policy_from_network(models[state.current_player()], state, args.postprocessing)
             actions, probabilities = [], []
             for action, probs in aps.items():
               actions.append(action)
@@ -67,11 +97,11 @@ def evaluate_network_to_random(args):
         state = network._game.new_initial_state()
         while not state.is_terminal(): 
           if state.current_player() == player:
-            aps = network.action_probabilities(state)
+            aps = get_policy_from_network(network, state, args.postprocessing)
             actions, probabilities = [], []
             for action, probs in aps.items():
               actions.append(action)
-              probabilities.append(probs)
+              probabilities.append(probs) 
             action = np_rng.choice(actions, p=probabilities)
           else:
             action = np_rng.choice(state.legal_actions())
